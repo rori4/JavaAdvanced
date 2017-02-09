197d8820723b980d0ea52511b67a010bdbe22815
rangel <RangelStoilov@students.softuni.bg>
2017-02-09 16:38:51 +0200

    Update on lessons and excesses

---
+++ src/StacksAndQueuesExercises/SimpleTextEditor.java
@@ -0,0 +1,40 @@
+package StacksAndQueuesExercises;
+
+import java.util.*;
+
+public class SimpleTextEditor {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Deque<String> lastStrings = new ArrayDeque<>();
+        String currentString = "";
+        lastStrings.push(currentString);
+        for (int i = 0; i < n; i++) {
+            String[] currentCommand = scanner.nextLine().split(" ");
+            switch (currentCommand[0]) {
+                case "1": {
+                    currentString += currentCommand[1];
+                    lastStrings.push(currentString);
+                }
+                break;
+                case "2": {
+                    int countOfLastEl = Integer.parseInt(currentCommand[1]);
+                    currentString = currentString.substring(0, currentString.length() - countOfLastEl);
+                    lastStrings.push(currentString);
+                }
+                break;
+                case "3": {
+                    int indexForPrint = Integer.parseInt(currentCommand[1]);
+                    System.out.println(currentString.charAt(indexForPrint - 1));
+                }
+                break;
+                case "4": {
+                    lastStrings.pop();
+                    currentString = lastStrings.peek();
+                }
+                break;
+            }
+        }
+    }
+}
--- src/StacksAndQueuesExercises/StackFibonacci.java
+++ src/StacksAndQueuesExercises/StackFibonacci.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
 
-public class RecursiveFibonacci {
+public class StackFibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
